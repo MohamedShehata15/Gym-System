73ac1e24487a008cd9d240a38996920b8d70b406
mohamed159716 <mohamed159716@yahoo.com>
2022-03-10 12:46:54 +0200

    Fixed Some issues

--- app/Http/Controllers/cityManagerController.php
+++ app/Http/Controllers/cityManagerController.php
@@ -6,43 +6,35 @@ use App\Models\Staff;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CityManagerController extends Controller
-{
-    public function index(){
-        if(Auth::user()->role=="city_manager"){
-           $cityManagerId= Auth::user()->id;
-           $cityGyms=Staff::find($cityManagerId)->city->gyms;
-           return view('cityManagers.index',[   
-               'cityGyms'=>$cityGyms
-           ]);  
-        }else{
-            dd("You aren't City Manager so you can't enter.. ");  
-        } 
-
+class CityManagerController extends Controller {
+    public function index() {
+        if (Auth::user()->role == "city_manager") {
+            $cityManagerId = Auth::user()->id;
+            $cityGyms = Staff::find($cityManagerId)->city->gyms;
+            return view('cityManagers.index', [
+                'cityGyms' => $cityGyms
+            ]);
+        } else {
+            dd("You aren't City Manager so you can't enter.. ");
+        }
     }
-    public function show(){
-        
+    public function show() {
     }
-    public function edit(){
-
+    public function edit() {
     }
-    public function store(){
-
+    public function store() {
     }
-    public function update(){
-
+    public function update() {
     }
-    public function create(){
-
+    public function create() {
     }
-    public function destroy(){
-
+    public function destroy() {
     }
-    public function ban($id){
-        $gymManager=Staff::where('id',$id)->first();
-        $isBan= $gymManager->is_baned;
-        $isBan == 0 ? $updatedBan =[  'is_baned' => 1] :  $updatedBan =[  'is_baned' => 0 ];
-        Staff::where('id',$id)->update($updatedBan);
+    public function ban($id) {
+        $gymManager = Staff::where('id', $id)->first();
+        $isBan = $gymManager->is_baned;
+        $isBan == 0 ? $updatedBan = ['is_baned' => 1] :  $updatedBan = ['is_baned' => 0];
+        Staff::where('id', $id)->update($updatedBan);
         return redirect()->route("cityManagers.index");
     }
 }
