033ee5efd249fc273eedf4b5ae6d3be2eb0aaab7
YaraMohammed98 <you@example.com>
2022-03-11 17:17:20 +0200

    change buy package tab

--- resources/views/gyms/show.blade.php
+++ resources/views/gyms/show.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container card card-primary card-outline w-50 text-dark">
         <div class="card-header ">
-          <h3 class="card-title">Gym Name:- {{$gym->name}}</h3>
+          <h3 class="card-title">Gym Name : {{$gym->name}}</h3>
           <div class="card-tools">
             <a href="{{route('gyms.index')}}"" class="badge badge-primary">Back</a>
           </div>
