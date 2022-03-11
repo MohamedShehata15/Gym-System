b7449db4a5cf88cbd811dfc1f47332991256992f
YaraMohammed98 <you@example.com>
2022-03-11 15:09:45 +0200

    edit aside tabs

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -16,6 +16,43 @@
 @yield('menubar')
 
 @if(Auth::user()->hasRole('Super-Admin'))
+<!--Users Tab-->
+<li class="nav-item has-treeview">
+            <a href=".multi-collapse" class="nav-link bg-warning text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+              <i class="nav-icon fas fa fa-user"></i>
+              <p>
+               Users
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('users.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List Users</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+<!--Cities Tab-->
+<li class="nav-item has-treeview">
+            <a href=".multi-collapse" class="nav-link bg-danger text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="CollapseExample1">
+              <i class="nav-icon fas fa fa-building"></i>
+              <p>
+               Cities
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('cities.index') }}" class="nav-link active  multi-collapse" id="CollapseExample1">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List Cities</p>
+                </a>
+              </li>
+              </ul>
+          </li>
 <!--City Manager Tab-->
 <li class="nav-item has-treeview">
             <a href=".multi-collapse" class="nav-link active text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
@@ -39,9 +76,36 @@
                 </a>
               </li>
             </ul>
+          </li>          
+<!--Coaches tab-->
+<li class="nav-item has-treeview">
+            <a href=".multi-collapse" class="nav-link bg-purple " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+              <i class="nav-icon fas fa fa-users"></i>
+              <p>
+              Coaches
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('coaches.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List Coaches</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{route('coaches.create')}}" class="nav-link active multi-collapse" id="multiCollapseExample2">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add a Coach</p>
+                </a>
+              </li>
+            </ul>
           </li>
+
+@endif
+@if(Auth::user()->hasRole('city_manager') || Auth::user()->hasRole('Super-Admin') )
 <!--GYm Managers Tab-->
-          <li class="nav-item has-treeview">
+<li class="nav-item has-treeview">
             <a href=".multi-collapse" class="nav-link bg-success" data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
               <i class="nav-icon fas fa fa-user"></i>
               <p>
@@ -64,42 +128,6 @@
               </li>
             </ul>
           </li>
-<!--Users Tab-->
-<li class="nav-item has-treeview">
-            <a href=".multi-collapse" class="nav-link bg-warning text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
-              <i class="nav-icon fas fa fa-user"></i>
-              <p>
-               Users
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('users.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>List Users</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-<!--Cities Tab-->
-<li class="nav-item has-treeview">
-            <a href=".multi-collapse" class="nav-link bg-danger text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="CollapseExample1">
-              <i class="nav-icon fas fa fa-building"></i>
-              <p>
-               Cities
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('cities.index') }}" class="nav-link active  multi-collapse" id="CollapseExample1">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>List Cities</p>
-                </a>
-              </li>
-              </ul>
-          </li>
 <!--Gyms Tab-->
 <li class="nav-item has-treeview">
             <a href=".multi-collapse" class="nav-link bg-black text-white" data-toggle="collapsing" aria-expanded="false" aria-controls="CollapseExample1">
@@ -120,6 +148,30 @@
                 <a href="{{route('gyms.create')}}" class="nav-link active multi-collapse" id="multiCollapseExample2">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Create new Gym</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+<!--Training Packages Tab-->
+<li class="nav-item has-treeview">
+            <a href=".multi-collapse" class="nav-link bg-secondary " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+              <i class="nav-icon fas fa fa-tags"></i>
+              <p>
+              Training Packages
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('training-packages.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List Packages</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{route('buypackage.create')}}" class="nav-link active multi-collapse" id="multiCollapseExample2">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Buy a training Package</p>
                 </a>
               </li>
             </ul>
@@ -148,73 +200,6 @@
               </li>
             </ul>
           </li>
-
-<!--Training Packages Tab-->
-<li class="nav-item has-treeview">
-            <a href=".multi-collapse" class="nav-link bg-secondary " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
-              <i class="nav-icon fas fa fa-tags"></i>
-              <p>
-              Training Packages
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('training-packages.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>List Packages</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('buypackage.create')}}" class="nav-link active multi-collapse" id="multiCollapseExample2">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Buy a training Package</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-<!--Coaches tab-->
-<li class="nav-item has-treeview">
-            <a href=".multi-collapse" class="nav-link bg-purple " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
-              <i class="nav-icon fas fa fa-users"></i>
-              <p>
-              Coaches
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('coaches.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>List Coaches</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('coaches.create')}}" class="nav-link active multi-collapse" id="multiCollapseExample2">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Add a Coach</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-<!--Attendance tab-->
-<li class="nav-item has-treeview">
-            <a href=".multi-collapse" class="nav-link bg-pink " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
-              <i class="nav-icon fas fa fa-check"></i>
-              <p>
-              Attendance
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('attendances.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Show Attendance</p>
-                </a>
-              </li>
-            </ul>
-          </li>
 <!--Revenue tab-->
 <li class="nav-item has-treeview">
             <a href=".multi-collapse" class="nav-link bg-yellow " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
@@ -232,42 +217,26 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>   
+<!--Attendance tab-->
+<li class="nav-item has-treeview">
+            <a href=".multi-collapse" class="nav-link bg-pink " data-toggle="collapsing" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+              <i class="nav-icon fas fa fa-check"></i>
+              <p>
+              Attendance
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('attendances.index') }}" class="nav-link active  multi-collapse" id="multiCollapseExample1">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Show Attendance</p>
+                </a>
+              </li>
+            </ul>
+          </li>                 
 @endif
-
-
-
-
-
-@if(Auth::user()->hasRole('city_manager'))
- <li class="w-100">
-          <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-              Gyms
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-            <div class="accordion-body">
-               <strong class="text-danger">
-                  
-                           <a href="{{ route('gyms.create') }}" class=" {{ Request::is('gyms.create') ? 'active' : '' }}">
-                            <i class="far fa-circle nav-icon"></i>
-                            <span>Add GYM</span>
-                           </a>
-                           <a href="{{ route('gyms.index') }}" class="nav-link {{ Request::is('gyms.index') ? 'active' : '' }}">
-                            <i class="nav-icon fas fa-home"></i>
-                            <p>ListGyms</p>
-                           </a>                   
-            </strong> 
-            </div>
-          </div>
-        </div>
-      </div>
- </li>
-@endif
-
-
 
 @if( Auth::user()->hasRole('coach'))
 <li class="nav-item">
